e46578a4b92f02234995f265a024dac35df90a92
sgyoung96 <sgyoung96@naver.com>
2023-05-25 07:56:11 +0900

    front-end 단 프로젝트 분리를 위해 기존 프로젝트에서 Vue 설치 파일 삭제

--- proj/proj/src/main/proj/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Header = () => {
-	return (
-		<>
-			<h1>헤더입니다.</h1>
-		</>	
-	);
-};
--- proj/proj/src/main/proj/src/config.js
+++
@@ -1,3 +0,0 @@
-const BASE_URL = process.env.REACT_APP_SERVER_HOST;
-
-export { BASE_URL };
